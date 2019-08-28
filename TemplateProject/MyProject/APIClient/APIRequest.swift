78d8486e14327f09ea73b2735face379e8d123dc
Raghvendra <raghvendrashri@Raghvendra-Mac.local>
2019-08-28 18:50:47 +0530

    Initial commit

---
+++ TemplateProject/MyProject/APIClient/APIRequest.swift
@@ -0,0 +1,53 @@
+//
+//  APIRoute.swift
+//  MyProject
+//
+//  Created by AuthorName
+//  Copyright © 2019 OrganizationName. All rights reserved.
+//
+
+import Foundation
+
+enum APIRequest {
+    
+    //MARK: Endopints
+    
+    // Add endpoints as enum cases
+    
+    //MARK: -
+    
+    private func httpProtocol() -> String {
+        return "https"
+    }
+    
+    private func basePath() -> String {
+        return ""
+    }
+    
+    private func path() -> String {
+        switch self {
+        default:
+            return ""
+        }
+    }
+    
+    private func method() -> String {
+        switch self {
+        default:
+            return "GET"
+        }
+    }
+    
+    private func url() -> URL? {
+        return URL(string: "\(self.httpProtocol())://\(self.basePath())\(self.path())")
+    }
+    
+    func request() -> URLRequest? {
+        if let url = self.url() {
+            var request: URLRequest = URLRequest(url: url)
+            request.httpMethod = self.method()
+            return request
+        }
+        return nil
+    }
+}
